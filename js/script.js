6a3a3351cc8466b20eff724b5a638dd63e26554a
ze <zelkatani@gmail.com>
2017-07-02 23:37:38 +0100

    Add design skeleton

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+let draw = document.querySelector("#draw"),
+    cvs = document.querySelector("canvas");
+cvs.width = draw.offsetWidth;
+
 let canvas = new Canvas();
 
 canvas.enableRect();
